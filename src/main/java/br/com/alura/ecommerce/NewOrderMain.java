aa47fbc816fd7660e9961e154de7d732defdcc5c
wtrindades <wtrindades@gmail.com>
2022-01-06 13:31:05 -0300

    feat: add EmailService with kafka consumer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -3,6 +3,7 @@ package br.com.alura.ecommerce;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,17 +13,24 @@ public class NewOrderMain {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		var producer = new KafkaProducer<String, String>(properties());
+
 		var value = "123, 23232, 3232656";
 		var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
 
-		producer.send(record, (data, ex) -> {
+		Callback callback = (data, ex) -> {
 			if(ex != null) {
 				ex.printStackTrace();
 				return;
 			}
 
 			System.out.println("success sending " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-		}).get();
+		};
+
+		var email = "Thank you for your order! We are processing your order!";
+		var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email);
+
+		producer.send(record, callback).get();
+		producer.send(emailRecord, callback).get();
 	}
 
 	private static Properties properties() {
