6bd3aac37e579dc513ff92666a689a7eb6ef13ba
wtrindades <wtrindades@gmail.com>
2022-01-20 17:46:00 -0300

    refactor: change EmailService to use KafkaService

--- src/main/java/br/com/alura/ecommerce/EmailService.java
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,55 +1,32 @@
 package br.com.alura.ecommerce;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
 
 	public static void main(String[] args) {
-		var consumer = new KafkaConsumer<String, String>(properties());
+		var emailService = new EmailService();
+		var service = new KafkaService(EmailService.class.getSimpleName(),
+				"ECOMMERCE_SEND_EMAIL", emailService::parse);
 
-		consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-
-		while(true) {
-			var records = consumer.poll(Duration.ofMillis(100));
-			if(!records.isEmpty()) {
-				System.out.println("Found " + records.count() + " records");
-
-				for(var record : records) {
-					System.out.println("----------------------------------------");
-					System.out.println("Send email");
-					System.out.println(record.key());
-					System.out.println(record.value());
-					System.out.println(record.partition());
-					System.out.println(record.offset());
-
-					try {
-						Thread.sleep(1000);
-					} catch (Exception e) {
-						// TODO: handle exception
-					}
-
-					System.out.println("Email sent");
-				}
-
-				continue;
-			}
-		}
+		service.run();
 	}
 
-	private static Properties properties() {
-		var properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+	public void parse(ConsumerRecord<String, String> record) {
+		System.out.println("----------------------------------------");
+		System.out.println("Send email");
+		System.out.println(record.key());
+		System.out.println(record.value());
+		System.out.println(record.partition());
+		System.out.println(record.offset());
 
-		return properties;
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		System.out.println("Email sent");
 	}
 
 }
